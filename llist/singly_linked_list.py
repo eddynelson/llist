81310073846ca320150e8272dbb10bc9a57820c1
Eddy Decena <eddynelson02@gmail.com>
2020-12-11 18:02:37 -0400

    Add delete_last method to Singly linked list implementation

--- llist/singly_linked_list.py
+++ llist/singly_linked_list.py
@@ -14,7 +14,7 @@ class SinglyLinkedList():
         return self._head
     
     @head.setter
-    def head(self, node: SinglyNode) -> None: 
+    def head(self, node: SinglyNode) -> None:
         if self.size == 0:
             self._head = node
         else:
@@ -34,11 +34,26 @@ class SinglyLinkedList():
     def tail(self) -> SinglyNode:
         return self._tail
     
-    def addFirst(self, node: SinglyNode) -> None:
+    def add_first(self, node: SinglyNode) -> None:
         temp_next = self._head.next
         node.next = temp_next
         self._head = node
+        self.size += 1
     
+    def delete_last(self):
+        __node = self._head
+        _node = __node
+        
+        while True:
+            if _node.next == None:
+                __node.next = None
+                break
+            
+            __node = _node
+            _node = _node.next
+        
+        self._tail = __node
+        self.size -= 1
     # def map(self, fn: Callable) -> None:
     #     node = self._head
         
